1706121cec0e0936f6a0b102cd0705ae2dd63461
Prakash Joshi <prakashjoshiversion1@gmail.com>
2015-11-28 19:07:11 +0545

    created a server

--- src/main/java/com/pcjoshi/chatter/ChattyServer.java
+++ src/main/java/com/pcjoshi/chatter/ChattyServer.java
@@ -1,11 +1,29 @@
 package com.pcjoshi.chatter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class ChattyServer extends AbstractVerticle {
-	
-	@Override
-	public void start(){
-		
-	}
+
+    @Override
+    public void start(Future<Void> fut) {
+        Router router = Router.router(vertx);
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/html")
+                    .end("<html><h1>Hello world<h1></html>");
+        });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080,result -> {
+            if (result.succeeded()){
+                fut.complete();
+                System.out.println("Successfull deployed server at port 8080");
+            }else {
+                fut.fail(result.cause());
+            }
+        });
+
+    }
 }
